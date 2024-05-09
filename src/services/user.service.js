4adbf20ed4670e94a046c816fbe502703afcecff
casstaal <casstaal@hotmail.com>
2024-05-09 10:30:16 +0200

    Created the local database connection from lesson 5. Tested it and it works now.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,6 @@
 const database = require('../dao/inmem-db')
-var logger = require('tracer').console()
+const db = require('../dao/mysql-db')
+const logger = require('tracer').console()
 
 const userService = {
     create: (user, callback) => {
@@ -16,16 +17,44 @@ const userService = {
     },
 
     getAll: (callback) => {
-        database.getAll((err, data) => {
+        logger.info('getAll')
+        // Deprecated: de 'oude' manier van werken, met de inmemory database
+        // database.getAll((err, data) => {
+        //     if (err) {
+        //         callback(err, null)
+        //     } else {
+        //         console.log(data)
+        //         callback(null, {
+        //             message: `Found ${data.length} users.`,
+        //             data: data
+        //         })
+        //     }
+        // })
+        // Nieuwe manier van werken: met de MySQL database
+        db.getConnection(function (err, connection) {
             if (err) {
+                logger.error(err)
                 callback(err, null)
-            } else {
-                console.log(data)
-                callback(null, {
-                    message: `Found ${data.length} users.`,
-                    data: data
-                })
+                return
             }
+
+            connection.query(
+                'SELECT id, firstName, lastName FROM `user`',
+                function (error, results, fields) {
+                    connection.release()
+
+                    if (error) {
+                        logger.error(error)
+                        callback(error, null)
+                    } else {
+                        logger.debug(results)
+                        callback(null, {
+                            message: `Found ${results.length} users.`,
+                            data: results
+                        })
+                    }
+                }
+            )
         })
     },
 
